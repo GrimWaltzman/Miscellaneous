46636804fcc9190aa8f0f512a7b1a0b22e81f539
GrimWaltzman <mymailforallstuff@gmail.com>
2020-01-21 17:17:19 +0200

    Initial commit

---
+++ xprs/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger')
+
+
+const app = express();
+
+//Init miiddleware
+// app.use(logger);
+
+
+// Body parser middleware
+app.use(express.json);
+app.use(express.urlencoded({extended: false}));
+
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>(console.log(`Server started on port ${PORT}`)));
+ 
